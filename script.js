267fc215852b874e54998e6ab0ed2d50de822eb9
irfan0701 <irfansetyadinata69@gmail.com>
2021-11-27 15:14:37 +0800

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Toogle Account
+
+var LoginForm = document.getElementById("LoginForm");
+var RegisterForm = document.getElementById("RegisterForm");
+var Indicator = document.getElementById("Indicator");
+
+function register() {
+
+  RegisterForm.style.transform = "translateX(300px)";
+  LoginForm.style.transform = "translateX(-300px)";
+  Indicator.style.transform = "translateX(100px)";
+}
+
+function login() {
+    
+    RegisterForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(0px)";
+}
+
 // Toogle Menu
 
 var MenuItems = document.getElementById("MenuItems");
